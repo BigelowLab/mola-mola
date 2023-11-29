c317655742e0263d2c2ee3dab6a1ac1aedb7dd0e
Ben Tupper <btupper@bigelow.org>
2023-11-28 19:09:26 -0500

    rebuild after nbs update

--- functions/stars.R
+++ functions/stars.R
@@ -5,11 +5,19 @@
 #' @param u_wind_db NULL or a database table
 #' @param v_wind_db NULL or a database table
 #' @param sst_path char, the path to sst data
-#' @param wind_path char, the path to wind data
+#' @param nbs_path char, the path to wind data
+#' @param nbs_shift numeric, days to shift nbs days by because nbs, natively,
+#'   is time-stamped to the middle of each month.  Ignored if per in db
+#'   is not "month"
 #' @return stars object with one or more attributes (variables)
 read_predictors = function(
-    sst_db = NULL, windspeed_db = NULL, u_wind_db = NULL, v_wind_db = NULL,
-    sst_path = "data/oisst", wind_path = "data/nbs"){
+    sst_db = NULL, 
+    windspeed_db = NULL, 
+    u_wind_db = NULL, 
+    v_wind_db = NULL,
+    sst_path = "data/oisst", 
+    nbs_path = "data/nbs2",
+    nbs_shift = -14){
   
   
   if (all(c(is.null(sst_db), is.null(windspeed_db), is.null(u_wind_db), is.null(v_wind_db)))){
@@ -22,11 +30,20 @@ read_predictors = function(
                 if (is.null(db[[name]])) return(NULL)
                 files = if(name == "sst"){
                   filenames = oisster::compose_filename(db[[name]], sst_path)
+                  x = stars::read_stars(filenames, along = list(time = db[[name]]$date)) |>
+                    rlang::set_names(name)
                 } else {
-                  filenames = nbs::compose_filename(db[[name]], wind_path)
+                  filenames = nbs::compose_filename(db[[name]], nbs_path)
+                  x = stars::read_stars(filenames, along = list(time = db[[name]]$date)) |>
+                    rlang::set_names(name)
+                  if (db[[name]]$per[1] == "month"){
+                    d = stars::st_dimensions(x)
+                    d$time$values = d$time$values + nbs_shift
+                    stars::st_dimensions(x) <- d
+                  }
                 }
-                stars::read_stars(filenames, along = list(time = db[[name]]$date)) |>
-                  rlang::set_names(name)
+
+                x
               })
     
   # purge any NULLS
